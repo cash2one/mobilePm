08bb53d15fa8cb8b73feec644bf753948619298c
tjdreams <15221806781@163.com>
2016-03-16 14:51:37 +0800

    弹出层 手机端更改

--- hb/js/comm.js
+++ hb/js/comm.js
@@ -20,26 +20,6 @@
     }
 })();
 
-/*左侧菜单*/
-var slideout = new Slideout({
-    'panel': document.getElementById('main'),
-    'menu': document.getElementById('menu'),
-    'padding': 256,
-    'tolerance': 70
-});
-
-document.querySelector('.js-slideout-toggle').addEventListener('click', function() {
-    slideout.toggle();
-});
-
-document.querySelector('.menu').addEventListener('click', function(eve) {
-    if (eve.target.nodeName === 'A') { slideout.close(); }
-});
-function aload(t){"use strict";t=t||window.document.querySelectorAll("[data-aload]"),void 0===t.length&&(t=[t]);var a,e=0,r=t.length;for(e;r>e;e+=1)a=t[e],a["LINK"!==a.tagName?"src":"href"]=a.getAttribute("data-aload"),a.removeAttribute("data-aload");return t}
-window.onload = function(){
-    aload();
-    document.querySelector('.iphone').className += ' shown';
-};
 /*返回顶部*/
 window.onload = function(){
     var oTop = document.getElementById("top-animation");
@@ -56,9 +36,9 @@ window.onload = function(){
     topDb();
     window.onscroll = function(){
         topDb();
-    }
+    };
     oTop.onclick = function(){
         document.documentElement.scrollTop = document.body.scrollTop =0;
-    }
+    };
 };
 
